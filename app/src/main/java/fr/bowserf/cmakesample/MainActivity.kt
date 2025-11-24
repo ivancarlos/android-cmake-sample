a621ed7f3ea98cbb5efb22949373098e57c96f9f
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-24 00:23:16 -0300

    :add: Adiciona o arquivo app/src/main/java/fr/bowserf/cmakesample/MainActivity.kt

---
+++ app/src/main/java/fr/bowserf/cmakesample/MainActivity.kt
@@ -0,0 +1,48 @@
+package fr.bowserf.cmakesample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val calculator = Calculator()
+
+        val tvResultComputation = findViewById<TextView>(R.id.tv_result_computation)
+        val editTextVal1 = findViewById<EditText>(R.id.edit_value1)
+        val editTextVal2 = findViewById<EditText>(R.id.edit_value2)
+
+        val operator = findViewById<TextView>(R.id.tv_multiply_symbol)
+
+        findViewById<Button>(R.id.btn_multiply).setOnClickListener {
+            val value1 = editTextVal1.text.toString().toLong()
+            val value2 = editTextVal2.text.toString().toLong()
+            val result = calculator.multiply(value1, value2)
+            tvResultComputation.text = result.toString()
+            operator.text = "x"
+        }
+
+        findViewById<Button>(R.id.btn_addition).setOnClickListener {
+            val value1 = editTextVal1.text.toString().toLong()
+            val value2 = editTextVal2.text.toString().toLong()
+            val result = calculator.add(value1, value2)
+            tvResultComputation.text = result.toString()
+            operator.text = "+"
+        }
+
+        findViewById<Button>(R.id.btn_subtraction).setOnClickListener {
+            val value1 = editTextVal1.text.toString().toLong()
+            val value2 = editTextVal2.text.toString().toLong()
+            val result = calculator.minus(value1, value2)
+            tvResultComputation.text = result.toString()
+            operator.text = "-"
+        }
+    }
+
+}
