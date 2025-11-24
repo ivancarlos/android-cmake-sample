4535617d992f1445e4945c3baaa3a8c45329ae1e
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-24 00:23:16 -0300

    :add: Adiciona o arquivo app/src/main/java/fr/bowserf/cmakesample/Calculator.kt

---
+++ app/src/main/java/fr/bowserf/cmakesample/Calculator.kt
@@ -0,0 +1,16 @@
+package fr.bowserf.cmakesample
+
+class Calculator {
+
+    external fun multiply(value1: Long, value2: Long): Long
+
+    external fun add(value1: Long, value2: Long): Long
+
+    external fun minus(value1: Long, value2: Long): Long
+
+    companion object {
+        init {
+            System.loadLibrary("calculator")
+        }
+    }
+}
